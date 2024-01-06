a1b9dfc40b2e75287e854d920a74c05d8b60a7b2
siren186 <765495939@qq.com>
2024-01-06 11:35:34 +0800

    消除编译警告

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     char key[] = "f79fa56a4cc1";
-    tinyotp::totp otp(key, strlen(key), 2);
+    tinyotp::totp otp(key, (int)strlen(key), 2);
     unsigned int code0 = otp.get_code(_time64(nullptr));
     unsigned int code1 = otp.get_code_by_step(1);
     return 0;
